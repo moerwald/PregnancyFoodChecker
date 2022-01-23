9cf3330e6817a37865ffb21e601a4b19624f2e64
Andreas Mewald <a.mewald@pke.at>
2022-01-23 08:29:15 +0100

    Initial commit

---
+++ src/PregnancyFoodCheckWpf/PregnancyFoodCheckWpf/Model/NotAllowedPregnanyFood.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PregnancyFoodCheckWpf.Model
+{
+    public class NotAllowedPregnanyFood : INotifyPropertyChanged
+    {
+
+        private string _foodName;
+        public string FoodName
+        {
+            get { return _foodName; }
+            set { 
+                _foodName = value;
+            }
+        }
+
+
+        private int _preWeekStart;
+        public int PregWeekStart
+        {
+            get { return _preWeekStart; }
+            set { 
+                _preWeekStart = value;
+            }
+        }
+
+        private int _preWeekEnd;
+        public int PregWeekEnd
+        {
+            get { return _preWeekEnd; }
+            set { 
+                _preWeekEnd = value;
+            }
+        }
+
+        private string _amount;
+        public string Amount
+        {
+            get { return _amount; }
+            set { 
+                _amount = value;
+            }
+        }
+
+
+
+
+    }
+}
---
+++ src/PregnancyFoodCheckWpf/PregnancyFoodCheckWpf/ViewModel/BaseViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace PregnancyFoodCheckWpf.ViewModel
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChagend([System.Runtime.CompilerServices.CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
